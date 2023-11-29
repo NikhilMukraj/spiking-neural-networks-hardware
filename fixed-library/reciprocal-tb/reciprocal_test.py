e942d78ae276abe0cf177ab0e34f8750a709c113
Nikhiljag <nikhilmukraj@gmail.com>
2023-11-28 19:14:16 -0500

    removing unnecessary logging

--- fixed-library/reciprocal-tb/reciprocal_test.py
+++ fixed-library/reciprocal-tb/reciprocal_test.py
@@ -12,14 +12,11 @@ import numpy as np
 async def reciprocal(dut):
     int_bits = 16
     frac_bits = 16
-    # bounds = 2 ** 15
     bounds = 128
-
-    with open('test.txt', 'w+') as f: f.write('')
 
     for i in range(100):
         a = np.random.randint(-bounds, bounds)
-        if a == 0:
+        if a == 0: # 0 not handled yet
             a = 1
 
         binary_a = decimal_to_fixed_point(a, int_bits, frac_bits)    
@@ -29,9 +26,6 @@ async def reciprocal(dut):
 
         output = str(dut.out.value)
         output_value = fixed_point_to_decimal(output, int_bits, frac_bits)
-
-        # with open('t.txt', 'w+') as f: f.write(f'{1 / a} | {output} | {output_value}')
-        with open('test.txt', 'a+') as f: f.write(f'{a} | {1 / a} | {output} | {output_value}\n')
 
         assert check_with_tolerance(
             1 / a,
