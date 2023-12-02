3c9c5df4943575394e0fc92a056e2db85b77157f
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-01 19:26:39 -0500

    local python fixed point package

---
+++ fixed-library/fixed-point-models/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+  
+
+requirements = ['numpy']
+
+setup(name='fixed_point_models',
+    version='0.1.0',
+    description='Modeling fixed point arithmetic',
+    packages=['fixed_point_models'],
+    install_requires=requirements
+)
