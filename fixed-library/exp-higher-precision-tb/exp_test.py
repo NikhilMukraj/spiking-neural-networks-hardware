555cde9e5b49e831e69ee206d1545b023a781ad0
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-14 19:12:53 -0500

    higher precision exp testing

---
+++ fixed-library/exp-higher-precision-tb/exp_test.py
@@ -0,0 +1,33 @@
+import cocotb
+from cocotb.triggers import FallingEdge, Timer
+from cocotb.binary import BinaryValue
+from fixed_point_models import fixed_point_to_decimal, decimal_to_fixed_point
+from fixed_point_models import adder_model, check_with_tolerance
+import numpy as np
+
+
+@cocotb.test()
+async def exp_test(dut):
+    int_bits = 16
+    frac_bits = 16
+    bounds = 31
+
+    for i in range(100):
+        x = np.random.randint(-bounds, bounds)
+
+        binary_x = decimal_to_fixed_point(x, int_bits, frac_bits)    
+        dut.x.value = BinaryValue(binary_x)
+
+        await Timer(2, units="ns")
+
+        output = str(dut.out.value)
+        assert 'x' not in output, f'{x} | {output}'
+        output_value = fixed_point_to_decimal(output, int_bits, frac_bits)
+
+        actual = np.exp(x)
+
+        assert check_with_tolerance(
+            actual,
+            output_value, 
+            1
+        ), f'{x} | {actual} | {output_value}'
