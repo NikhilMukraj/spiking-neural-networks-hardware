bd4b7cc277ecb5b6f55ce4cadc8e96eaf945d115
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-19 16:59:37 -0500

    fixed up izhikevich neuron to work on clk cycle, does not do timestep properly need to add * dt/t

--- izhikevich-core/izhikevich-tb/izhikevich.sv
+++ izhikevich-core/izhikevich-tb/izhikevich.sv
@@ -7,6 +7,7 @@ module izhikevich_core #(
 	parameter N=32,
 	parameter Q=16
 )(
+    input clk,
 	input [N-1:0] i,
 	input [N-1:0] v_init,
 	input [N-1:0] w_init,
@@ -24,48 +25,40 @@ module izhikevich_core #(
     output reg [N-1:0] w
 );
     reg eq, gt, lt, apply_edge;
-    reg [N-1:0] dv, dw, new_voltage, new_w, actual_voltage, actual_w;
+    reg [N-1:0] dv, dw, new_voltage, new_w;
 
-    fixed_point_cmp threshold ( actual_voltage, v_th, eq, gt, lt );
+    fixed_point_cmp threshold ( voltage, v_th, eq, gt, lt );
 
-    calc_dv calc_dv1 ( actual_voltage, w, i, dv );
+    calc_dv calc_dv1 ( voltage, w, i, dv );
     calc_dw calc_dw1 (
         a,
         b,
         c,
         d,
-        actual_voltage,
-        actual_w,
+        voltage,
+        w,
         dt,
         t,
         dw
     );
 
-    add adder1 ( actual_voltage, dv, new_voltage );
-    add adder2 ( actual_w, dw, new_w );
+    add adder1 ( voltage, dv, new_voltage );
+    add adder2 ( w, dw, new_w );
 
-    always @ (*) begin
+    always @ (posedge clk) begin
         if (rst) begin
-            actual_voltage = v_init;
-            actual_w = w_init;
             voltage = v_init;
             w = w_init;
         end
-        else if (!apply) begin
-            apply_edge <= 1; 
-        end
-        else if (apply_edge) begin
-            if (eq | gt) begin
-                actual_voltage <= c;
-                actual_w <= d;
-            end else if (apply) begin
-                actual_voltage <= new_voltage;
-                actual_w <= new_w;
-            end
-            apply_edge <= 0; 
-        end
 
-        voltage <= actual_voltage;
-        w <= actual_w;
+        if (apply) begin
+            if (eq | gt) begin
+                voltage <= c;
+                w <= d;
+            end else begin
+                voltage <= new_voltage;
+                w <= new_w;
+            end
+        end
     end
 endmodule
