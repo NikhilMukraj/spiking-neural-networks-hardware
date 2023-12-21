baaad79939731bf696a8e3e2642f67c637dde0db
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-21 17:09:40 -0500

    formatting

--- fixed-library/mult-tb/mult_test.py
+++ fixed-library/mult-tb/mult_test.py
@@ -10,11 +10,12 @@ import numpy as np
 async def mult_test(dut):
     int_bits = 16
     frac_bits = 16
-    bounds = 5
+    lower_bound = -5
+    upper_bound = 5
 
     for i in range(100):
-        a = np.random.uniform(-bounds, bounds)
-        b = np.random.uniform(-bounds, bounds)
+        a = np.random.uniform(-lower_bound, upper_bound)
+        b = np.random.uniform(-lower_bound, upper_bound)
 
         a = fixed_point_to_decimal(decimal_to_fixed_point(a, int_bits, frac_bits), int_bits, frac_bits)
         b = fixed_point_to_decimal(decimal_to_fixed_point(b, int_bits, frac_bits), int_bits, frac_bits)
