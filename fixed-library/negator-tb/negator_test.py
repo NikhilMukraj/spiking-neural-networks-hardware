2a0a38787291258ea2ea0fa3dc340fefe67db22b
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-22 20:10:50 -0500

    added 0 case

--- fixed-library/negator-tb/negator_test.py
+++ fixed-library/negator-tb/negator_test.py
@@ -6,6 +6,21 @@ from fixed_point_models import check_with_tolerance
 import numpy as np
 
 
+async def test_negator(a, dut, int_bits, frac_bits):
+    result = a * -1
+
+    dut.a.value = BinaryValue(decimal_to_fixed_point(a, int_bits, frac_bits))
+
+    await Timer(2, units="ns")
+
+    output = fixed_point_to_decimal(str(dut.out.value), int_bits, frac_bits)
+
+    result_bits = decimal_to_fixed_point(result, 16, 16)
+
+    assert check_with_tolerance(output, result, 1e-3), \
+    f'''{i} : {output} != -1 * {a} | {result} |
+    {str(dut.out.value)} vs {result_bits} from {str(dut.a.value)}'''
+
 @cocotb.test()
 async def negator_test(dut):
     int_bits = 16
@@ -15,19 +30,11 @@ async def negator_test(dut):
 
     for i in range(100):
         a = np.random.uniform(-lower_bound, upper_bound)
-
         a = fixed_point_to_decimal(decimal_to_fixed_point(a, int_bits, frac_bits), int_bits, frac_bits)
 
-        result = a * -1
+        await test_negator(a, dut, int_bits, frac_bits)
 
-        dut.a.value = BinaryValue(decimal_to_fixed_point(a, int_bits, frac_bits))
+    a = 0
+    a = fixed_point_to_decimal(decimal_to_fixed_point(a, int_bits, frac_bits), int_bits, frac_bits)
 
-        await Timer(2, units="ns")
-
-        output = fixed_point_to_decimal(str(dut.out.value), int_bits, frac_bits)
-
-        result_bits = decimal_to_fixed_point(result, 16, 16)
-
-        assert check_with_tolerance(output, result, 1e-3), \
-        f'''{i} : {output} != -1 * {a} | {result} |
-        {str(dut.out.value)} vs {result_bits} from {str(dut.a.value)}'''
+    await test_negator(a, dut, int_bits, frac_bits)
