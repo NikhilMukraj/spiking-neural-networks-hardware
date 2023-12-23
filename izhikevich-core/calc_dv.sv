3f9ae896a1f98c03e5217231821d3552e2ad3405
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-22 20:11:02 -0500

    implemented with negator circuit

--- izhikevich-core/calc_dv.sv
+++ izhikevich-core/calc_dv.sv
@@ -14,9 +14,9 @@ module calc_dv #(
 	mult multiplier2 ( 32'b00000000000000000000101000111101, term1, term2 ); // 0.04*(v*v)
 	mult multiplier3 ( 32'b00000000000001010000000000000000, v, term3 ); // 5*v
 	add adder1 ( term2, term3, term4 ); // (0.04*(v*v))+(5*v)
-	mult multiplier4 ( 32'b10000000000000010000000000000000, w, term5 ); // -1*w
+	negator negator4 ( w, term5 ); // -1*w
 	add adder2 ( term4, 32'b00000000100011000000000000000000, term6 ); // ((0.04*(v*v))+(5*v))+140
 	add adder3 ( term5, i, term7 ); // (-1*w)+i
 	add adder4 ( term6, term7, term8 ); // (((0.04*(v*v))+(5*v))+140)+((-1*w)+i)
-	mult multiplier5 ( term8, step, out ); // ((((0.04*(v*v))+(5*v))+140)+((-1*w)+i))*step
+	mult multiplier4 ( term8, step, out ); // ((((0.04*(v*v))+(5*v))+140)+((-1*w)+i))*step
 endmodule
--- izhikevich-core/calc_dw.sv
+++ izhikevich-core/calc_dw.sv
@@ -14,8 +14,8 @@ module calc_dw #(
 	reg [N-1:0] term1, term2, term3, term4;
 
 	mult multiplier1 ( b, v, term1 ); // b*v
-	mult multiplier2 ( 32'b10000000000000010000000000000000, w, term2 ); // -1*w
+	negator negator2 ( w, term2 ); // -1*w
 	add adder1 ( term1, term2, term3 ); // (b*v)+(-1*w)
-	mult multiplier3 ( a, term3, term4 ); // a*((b*v)+(-1*w))
-	mult multiplier4 ( term4, step, out ); // (a*((b*v)+(-1*w)))*step
+	mult multiplier2 ( a, term3, term4 ); // a*((b*v)+(-1*w))
+	mult multiplier3 ( term4, step, out ); // (a*((b*v)+(-1*w)))*step
 endmodule
