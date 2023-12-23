9b3a18a80869aeb93e842ff96cae719f68376bd2
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-23 16:22:54 -0500

    alchitry echo uart

--- uart/uart-test-alchitry-xilinx/echo_uart.v
+++ uart/uart-test-alchitry-xilinx/echo_uart.v
@@ -16,15 +16,14 @@
 
 module impl_top (
     input               clk     , // Top level system clock input.
-    input               sw_0    , // Slide switches.
-    input               sw_1    , // Slide switches.
+    input               rst    , // Slide switches.
     input   wire        uart_rxd, // UART Recieve pin.
     output  wire        uart_txd, // UART transmit pin.
     output  wire [7:0]  led
     );
     
     // Clock frequency in hertz.
-    parameter CLK_HZ = 50000000;
+    parameter CLK_HZ = 50000000; // could change to 100000000
     parameter BIT_RATE =   9600;
     parameter PAYLOAD_BITS = 8;
     
@@ -63,7 +62,7 @@ module impl_top (
     .CLK_HZ  (CLK_HZ  )
     ) i_uart_rx(
     .clk          (clk          ), // Top level system clock input.
-    .resetn       (sw_0         ), // Asynchronous active low reset.
+    .resetn       (!rst         ), // Asynchronous active low reset.
     .uart_rxd     (uart_rxd     ), // UART Recieve pin.
     .uart_rx_en   (1'b1         ), // Recieve enable
     .uart_rx_break(uart_rx_break), // Did we get a BREAK message?
@@ -80,7 +79,7 @@ module impl_top (
     .CLK_HZ  (CLK_HZ  )
     ) i_uart_tx(
     .clk          (clk          ),
-    .resetn       (sw_0         ),
+    .resetn       (!rst         ),
     .uart_txd     (uart_txd     ),
     .uart_tx_en   (uart_tx_en   ),
     .uart_tx_busy (uart_tx_busy ),
