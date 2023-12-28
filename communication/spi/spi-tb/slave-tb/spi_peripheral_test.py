ec8e8fcd7f8131aaa7ae0e9524856ebea686edcb
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-27 22:56:58 -0500

    tests for spi

--- communication/spi/spi-tb/slave-tb/spi_peripheral_test.py
+++ communication/spi/spi-tb/slave-tb/spi_peripheral_test.py
@@ -14,10 +14,23 @@ async def generate_clock(dut, timesteps):
 @cocotb.test()
 async def spi_peripheral_test(dut):
     await cocotb.start(generate_clock(dut, 100))
+    # redo this but start sck and clk clocks
+    # wait for dut.sck
+
+    dut.rst.value = BinaryValue(str('1'))
+    await RisingEdge(dut.clk) 
+
+    dut.rst.value = BinaryValue(str('0'))
+    await RisingEdge(dut.clk) 
+
+    dut.ss.value = BinaryValue(str('0'))
+    dut.miso.value = BinaryValue(str('1'))
+    await RisingEdge(dut.clk) 
 
     bit_string = '10101010'
 
     for i in bit_string:
+        await RisingEdge(dut.clk) 
         dut.miso.value = BinaryValue(str(i))
         dut._log.info(f'input value: {dut.miso.value}')
         await RisingEdge(dut.clk) 
