20d93376c05de50d8f3fbc776f7ed1ee2bef5133
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-28 21:19:01 -0500

    sck clock, found where problem is, dont know how to fix

--- communication/spi/spi-tb/slave-tb/spi_peripheral_test.py
+++ communication/spi/spi-tb/slave-tb/spi_peripheral_test.py
@@ -11,28 +11,42 @@ async def generate_clock(dut, timesteps):
         dut.clk.value = 1
         await Timer(4, units='ns')
 
+async def generate_spi_clock(dut, timesteps):
+    for cycle in range(timesteps):
+        dut.sck.value = 0
+        await Timer(4, units='ns')
+        dut.sck.value = 1
+        await Timer(4, units='ns')
+
 @cocotb.test()
 async def spi_peripheral_test(dut):
     await cocotb.start(generate_clock(dut, 100))
-    # redo this but start sck and clk clocks
-    # wait for dut.sck
+    await cocotb.start(generate_spi_clock(dut, 100))
 
     dut.rst.value = BinaryValue(str('1'))
-    await RisingEdge(dut.clk) 
+    await RisingEdge(dut.sck) 
 
     dut.rst.value = BinaryValue(str('0'))
-    await RisingEdge(dut.clk) 
+    await RisingEdge(dut.sck) 
 
     dut.ss.value = BinaryValue(str('0'))
-    dut.miso.value = BinaryValue(str('1'))
-    await RisingEdge(dut.clk) 
+    dut.mosi.value = BinaryValue(str('1'))
+    await RisingEdge(dut.sck) 
+
+    dut._log.info(f'select value: {dut.ss.value}')
+    dut._log.info(f'select_d value: {dut.ss_d.value}')
+    await RisingEdge(dut.sck) 
 
     bit_string = '10101010'
 
     for i in bit_string:
-        await RisingEdge(dut.clk) 
-        dut.miso.value = BinaryValue(str(i))
-        dut._log.info(f'input value: {dut.miso.value}')
-        await RisingEdge(dut.clk) 
+        # await RisingEdge(dut.clk) 
+        dut.mosi.value = BinaryValue(str(i))
+        dut._log.info(f'input value: {dut.mosi.value}')
+        dut._log.info(f'data_q value: {dut.data_q.value}')
+        dut._log.info(f'sck_q value: {dut.sck_q.value}')
+        dut._log.info(f'sck_d value: {dut.sck_d.value}')
+        dut._log.info(f'sck value: {dut.sck.value}')
+        await RisingEdge(dut.sck) 
 
-    assert str(dut.led.value) == bit_string, f'{str(dut.led.value)} != {bit_string}'
+    assert str(dut.dout.value) == bit_string, f'{str(dut.dout.value)} != {bit_string}'
