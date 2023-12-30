6f47b5433ee29a3ee357c14276c7f523bc721684
Nikhiljag <nikhilmukraj@gmail.com>
2023-12-29 21:17:51 -0500

    test update

--- communication/spi/spi-tb/slave-tb/spi_peripheral_test.py
+++ communication/spi/spi-tb/slave-tb/spi_peripheral_test.py
@@ -1,5 +1,5 @@
 import cocotb
-from cocotb.triggers import RisingEdge, Timer
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
 from cocotb.binary import BinaryValue
 import numpy as np
 
@@ -11,6 +11,9 @@ async def generate_clock(dut, timesteps):
         dut.clk.value = 1
         await Timer(4, units='ns')
 
+# if always (*) is replaced with always (posedge clk) and 
+# timer for sck is not a multiple of clk,
+# bits are shifted in
 async def generate_spi_clock(dut, timesteps):
     for cycle in range(timesteps):
         dut.sck.value = 0
@@ -24,29 +27,31 @@ async def spi_peripheral_test(dut):
     await cocotb.start(generate_spi_clock(dut, 100))
 
     dut.rst.value = BinaryValue(str('1'))
+    dut.ss.value = BinaryValue(str('1'))
     await RisingEdge(dut.sck) 
 
     dut.rst.value = BinaryValue(str('0'))
     await RisingEdge(dut.sck) 
 
-    dut.ss.value = BinaryValue(str('0'))
+    # dut.ss.value = BinaryValue(str('0'))
     dut.mosi.value = BinaryValue(str('1'))
-    await RisingEdge(dut.sck) 
+    await FallingEdge(dut.sck) 
 
-    dut._log.info(f'select value: {dut.ss.value}')
-    dut._log.info(f'select_d value: {dut.ss_d.value}')
-    await RisingEdge(dut.sck) 
+    # dut._log.info(f'select value: {dut.ss.value}')
+    # dut._log.info(f'data value: {dut.data.value}')
+    # await FallingEdge(dut.sck) 
 
     bit_string = '10101010'
 
-    for i in bit_string:
-        # await RisingEdge(dut.clk) 
+    for step, i in enumerate(bit_string):
+        # await FallingEdge(dut.sck) 
+        dut.ss.value = BinaryValue(str('0'))
         dut.mosi.value = BinaryValue(str(i))
-        dut._log.info(f'input value: {dut.mosi.value}')
-        dut._log.info(f'data_q value: {dut.data_q.value}')
-        dut._log.info(f'sck_q value: {dut.sck_q.value}')
-        dut._log.info(f'sck_d value: {dut.sck_d.value}')
-        dut._log.info(f'sck value: {dut.sck.value}')
-        await RisingEdge(dut.sck) 
+        await FallingEdge(dut.sck) 
+        dut._log.info(f'{step} | input value: {dut.mosi.value}')
+        dut._log.info(f'{step} | data value: {dut.data.value}')
+        dut._log.info(f'{step} | count value: {dut.bit_count.value}')
+        dut._log.info(f'{step} | done value: {dut.done.value}')
 
-    assert str(dut.dout.value) == bit_string, f'{str(dut.dout.value)} != {bit_string}'
+    assert str(dut.dout.value) == bit_string and str(dut.done.value) == '1', \
+    f'{str(dut.dout.value)} != {bit_string}'
