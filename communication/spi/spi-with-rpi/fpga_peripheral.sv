fc5881541eda2906b5be9e5788b6f51baad56b4b
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-02 18:36:07 -0500

    led peripheral

---
+++ communication/spi/spi-with-rpi/fpga_peripheral.sv
@@ -0,0 +1,24 @@
+module led_peripheral(
+    input clk,
+    input rst,
+    input sck,
+    input ss,
+    input mosi,
+    output miso,
+    output reg [7:0] led,
+);
+    reg done_rx, done_tx;
+
+    spi_peripheral spi_peripheral1(
+        .clk(clk),
+        .rst(rst),
+        .ss(ss),
+        .mosi(mosi),
+        .miso(miso)
+        .sck(sck),
+        .done_rx(done_rx),
+        .done_tx(done_tx),
+        .din(8'b00000000),
+        .dout(led)
+    );
+endmodule
