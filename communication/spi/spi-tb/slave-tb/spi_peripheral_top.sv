6765ef143ee6e0055a6eacd8c803b0fac476e377
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-01 19:03:25 -0500

    peripheral transmission impl

--- communication/spi/spi-tb/slave-tb/spi_peripheral_top.sv
+++ communication/spi/spi-tb/slave-tb/spi_peripheral_top.sv
@@ -14,12 +14,11 @@ module spi_peripheral(
 	output reg [7:0] dout
 );
 	reg [2:0] bit_count_rx, bit_count_tx;
-	reg [7:0] data_rx, data_tx;
+	reg [7:0] data_rx;
 
-	// impl transmission
 	always @ (posedge sck) begin 
 		if (ss) begin
-			bit_count <= 3'b000;
+			bit_count_rx <= 3'b000;
 		end else begin
 			bit_count_rx <= bit_count_rx + 1'b1;
 			data_rx = {data_rx[6:0], mosi};
@@ -32,14 +31,28 @@ module spi_peripheral(
 		end
 	end
 
+	always @ (negedge sck) begin
+		if (ss) begin
+			bit_count_tx <= 3'b000;
+		end else begin
+			bit_count_tx <= bit_count_tx + 1'b1;
+			miso <= din[~bit_count_tx];
+			if (bit_count_tx == 3'b111) begin 
+				done_tx <= 1'b1;
+			end else begin
+				done_tx <= 1'b0;
+			end
+		end
+	end
+
 	always @ (*) begin
 		if (rst) begin
 			done_tx <= 1'b0;
 			done_rx <= 1'b0;
 			bit_count_tx <= 3'b000;
 			bit_count_rx <= 3'b000;
-			data_tx <= 8'b00000000;
 			data_rx <= 8'b00000000;
+			miso <= 1'b0;
 		end
 	end
 endmodule
