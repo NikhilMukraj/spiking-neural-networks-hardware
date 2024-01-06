f660e8d5933563235528b0ee0a926d373e99f3b2
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-06 16:58:57 -0500

    tb change

--- communication/spi/spi-4-byte-reciever/bytes_reciever_test.py
+++ communication/spi/spi-4-byte-reciever/bytes_reciever_test.py
@@ -30,7 +30,8 @@ async def test_bit_string_recieve(dut, bit_string):
         dut.mosi.value = BinaryValue(str(i))
         await FallingEdge(dut.sck) 
         dut._log.info(f'{step} | input value: {dut.mosi.value}')
-        dut._log.info(f'{step} | data value: {dut.data.value}')
+        # dut._log.info(f'{step} | data value: {dut.data.value}')
+        dut._log.info(f'{step} | temp_out value: {dut.temp_out.value}')
         dut._log.info(f'{step} | bytes_count value: {dut.bytes_count.value}')
         dut._log.info(f'{step} | done_bytes value: {dut.done_bytes.value}')
 
@@ -42,15 +43,14 @@ async def spi_peripheral_test(dut):
     await cocotb.start(generate_clock(dut, 100))
     await cocotb.start(generate_spi_clock(dut, 100))
 
-    dut.rst.value = BinaryValue(str('1'))
-    dut.ss.value = BinaryValue(str('1'))
+    dut.rst.value = BinaryValue(str(1))
+    dut.ss.value = BinaryValue(str(1))
     await RisingEdge(dut.sck) 
 
-    dut.rst.value = BinaryValue(str('0'))
+    dut.rst.value = BinaryValue(str(0))
     await RisingEdge(dut.sck) 
 
-    dut.mosi.value = BinaryValue(str('1'))
+    dut.mosi.value = BinaryValue(str(1))
     await FallingEdge(dut.sck) 
 
-    await test_bit_string_recieve(dut, '10101010')
-    await test_bit_string_recieve(dut, '11110000')
+    await test_bit_string_recieve(dut, '10101010111100000000111101010101')
