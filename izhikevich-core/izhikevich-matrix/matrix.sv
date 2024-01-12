265806f850d8c0f979e9b901795ee789ac9ca3c1
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-12 17:39:59 -0500

    matrix start

---
+++ izhikevich-core/izhikevich-matrix/matrix.sv
@@ -0,0 +1,11 @@
+// specify inputs to a given node
+// each node that can connect to another node has a signal
+// if on, then that node is considered inputted
+// could feed these inputs into an aggregator of sorts
+
+// or could look at a crossbar impl
+
+module matrix_flow(
+
+);
+endmodule
