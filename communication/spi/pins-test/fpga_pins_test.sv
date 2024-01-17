bc10461a936ae4bbd8d9f2c76a4bb33981997fdf
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-17 15:20:00 -0500

    verilog pin test

---
+++ communication/spi/pins-test/fpga_pins_test.sv
@@ -0,0 +1,18 @@
+module top(
+    input in1,
+    input in2,
+    output reg [7:0] leds
+);
+    always @ (*) begin
+        if (in1) begin
+            leds[7:4] = 1'b1111;
+        end else begin
+            leds[7:4] = 1'b0000;
+        end
+        if (in2) begin 
+            leds[3:0] = 1'b0000;
+        end else begin
+            leds[3:0] = 1'b0000;
+        end
+    end
+endmodule
