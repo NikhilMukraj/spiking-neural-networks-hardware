cd41ffd9e411b27adb255f9cf5d042c9375c0e76
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-18 15:13:53 -0500

    ram models

--- ram/ram-model/ram_model.py
+++ ram/ram-model/ram_model.py
@@ -1,2 +1,6 @@
 # https://github.com/alexforencich/cocotbext-axi
-# ram model
+
+# https://www.chipverify.com/verilog/verilog-single-port-ram
+# https://github.com/ChloeeeYoo/Verilog_simplemodule/blob/main/01.Memory/ram.v
+# https://alchitry.com/sdram-verilog contains ram_test tb
+# convert verilog ram model to cocotb
