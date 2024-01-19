b6b8aecb0d63fa215743b6aa1b5a3f5c34d47f24
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-19 11:50:38 -0500

    start of tb

---
+++ ram/ram-tb/controller_test.py
@@ -0,0 +1,19 @@
+import cocotb
+from cocotb.triggers import FallingEdge, RisingEdge, Timer
+from cocotb.binary import BinaryValue
+import numpy as np
+
+
+async def generate_clock(dut, timesteps):
+    for cycle in range(timesteps):
+        dut.clk.value = 0
+        await Timer(4, units='ns')
+        dut.clk.value = 1
+        await Timer(4, units='ns')
+
+@cocotb.test
+def ram_model_test(dut):
+    await generate_clock(dut, 100)
+
+    raise NotImplementedError("Example unfinished")
+    
