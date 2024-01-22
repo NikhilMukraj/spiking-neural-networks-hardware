95c32f776dff03b96c87e9fff730aebd780fc177
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-22 14:59:51 -0500

    syntax fix

--- ram/ram-tb/controller_test.py
+++ ram/ram-tb/controller_test.py
@@ -11,8 +11,8 @@ async def generate_clock(dut, timesteps):
         dut.clk.value = 1
         await Timer(4, units='ns')
 
-@cocotb.test
-def ram_model_test(dut):
+@cocotb.test()
+async def ram_model_test(dut):
     await generate_clock(dut, 100)
 
     raise NotImplementedError('Example unfinished')
