177ba184a512e83df68c634e18b7bccd24160531
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-22 15:05:54 -0500

    low rst

--- communication/spi/pins-test/fpga_pins_test.sv
+++ communication/spi/pins-test/fpga_pins_test.sv
@@ -1,20 +1,23 @@
 module top(
     input in1,
     input in2,
+    input rst_n,
     output reg [7:0] leds
 );
-    assign leds = 8'b00000000;
-
     always @ (*) begin
+        if (!rst_n) begin
+            leds = 8'b00000000;
+        end
+
         if (in1) begin
-            leds[7:4] = 1'b1111;
+            leds[7:4] = 4'b1111;
         end else begin
-            leds[7:4] = 1'b0000;
+            leds[7:4] = 4'b0000;
         end
         if (in2) begin 
-            leds[3:0] = 1'b1111;
+            leds[3:0] = 4'b1111;
         end else begin
-            leds[3:0] = 1'b0000;
+            leds[3:0] = 4'b0000;
         end
     end
 endmodule
