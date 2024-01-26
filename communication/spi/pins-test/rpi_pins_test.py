dfb4a15395faa545b3d709a9866fd5c259de9934
Nikhiljag <nikhilmukraj@gmail.com>
2024-01-26 15:00:42 -0500

    reads from toml

--- communication/spi/pins-test/rpi_pins_test.py
+++ communication/spi/pins-test/rpi_pins_test.py
@@ -1,9 +1,16 @@
 import RPi.GPIO as GPIO
+import toml
 import time
 
 
+with open('../pins.toml', 'r') as f:
+    pins = toml.load(f)
+
+in1 = int(pins['pins']['in1_pin'])
+in2 = int(pins['pins']['in2_pin'])
+
 for i in range(100):
     time.sleep(1)
-    GPIO.output(23, i % 2)
-    GPIO.output(24, (i + 1) % 2)
+    GPIO.output(in1, i % 2)
+    GPIO.output(in2, (i + 1) % 2)
     
