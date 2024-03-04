0c5fa858d5203812cb19888b0e1375d50c23f02d
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-04 07:28:27 -0500

    stdp planning

---
+++ izhikevich-core/stdp/stdp-tb/stdp_test.py
@@ -0,0 +1,4 @@
+# fit ae^tx to linear piecewise functions, at split = -0.5 and 0.5
+# reduce range of t change to -1 to 1, beyond t change of -200 to 200 is capped or parameterizable
+# range reduction by division or lookup of msb
+# after fitting, seperate a and t functions may not be necessary
