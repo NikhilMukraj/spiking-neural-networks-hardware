81de04437b4bba444a86e96d19927ae357cc0793
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-13 15:48:42 -0400

    note

--- izhikevich-core/izhikevich-tb/plot_izhikevich_activity.py
+++ izhikevich-core/izhikevich-tb/plot_izhikevich_activity.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 df = pd.read_csv('output.log')
+# spiking is noted right after event occurs
 spike_times = [(i[0]-1, df['voltage'][i[0]-1]) for i in df.iterrows() if i[1]['is_spiking'] == 1]
 
 sns.set_theme(style='darkgrid')
