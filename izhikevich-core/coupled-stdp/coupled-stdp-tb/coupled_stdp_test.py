eef7552680762029ed20a64563a9f3b4ebaa081c
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-15 16:14:55 -0400

    default values

--- izhikevich-core/coupled-stdp/coupled-stdp-tb/coupled_stdp_test.py
+++ izhikevich-core/coupled-stdp/coupled-stdp-tb/coupled_stdp_test.py
@@ -17,4 +17,27 @@ async def generate_clock(dut, timesteps):
 
 @cocotb.test()
 async def coupled_test(dut):
-    pass
+    i = 40
+    v_init = -65
+    w_init = 30
+    v_th = 30
+    step = 0.5 / 10 # dt / tau_m
+    a = 0.01
+    b = 0.25
+    c = -55
+    d = 8.0
+    apply = 0
+    rst = 1
+
+    dut.i.value = BinaryValue(decimal_to_fixed_point(i, int_bits, frac_bits))
+    dut.v_init.value = BinaryValue(decimal_to_fixed_point(v_init, int_bits, frac_bits))
+    dut.w_init.value = BinaryValue(decimal_to_fixed_point(w_init, int_bits, frac_bits))
+    dut.v_th.value = BinaryValue(decimal_to_fixed_point(v_th, int_bits, frac_bits))
+    dut.step.value = BinaryValue(decimal_to_fixed_point(step, int_bits, frac_bits))
+    dut.a.value = BinaryValue(decimal_to_fixed_point(a, int_bits, frac_bits))
+    dut.b.value = BinaryValue(decimal_to_fixed_point(b, int_bits, frac_bits))
+    dut.c.value = BinaryValue(decimal_to_fixed_point(c, int_bits, frac_bits))
+    dut.d.value = BinaryValue(decimal_to_fixed_point(d, int_bits, frac_bits))
+
+    dut.dt_reciprocal.value = BinaryValue(decimal_to_fixed_point(1 / dt, int_bits, frac_bits))
+    dut.cm_reciprocal.value = BinaryValue(decimal_to_fixed_point(1, int_bits, frac_bits))
