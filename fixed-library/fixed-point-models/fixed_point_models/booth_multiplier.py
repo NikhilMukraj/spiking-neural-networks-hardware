f439131c22008fcf4f61a25e88b59718eae37cab
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-26 11:36:32 -0400

    booth algo simulation

--- fixed-library/fixed-point-models/fixed_point_models/booth_multiplier.py
+++ fixed-library/fixed-point-models/fixed_point_models/booth_multiplier.py
@@ -1,6 +1,11 @@
 # https://www.geeksforgeeks.org/computer-organization-booths-algorithm/
 # implement booth mulitplier in python and then in hardware
-from models import fixed_point_to_decimal, decimal_to_fixed_point
+if __name__ == '__main__':
+    from models import fixed_point_to_decimal, decimal_to_fixed_point
+else:
+    from .models import fixed_point_to_decimal, decimal_to_fixed_point
+import logging
+import sys
 
 
 def bit_add(m: str, n: str, length: int):
@@ -49,45 +54,76 @@ def bit_shift(n: str, shift: int) -> str:
         n_ = n + ''.join(['0'] * (-shift))
         return n_[-len(n):]
 
-def booth_algo(m, r, length):
+def booth_algo(m: int, r: int, length: int, debug: bool=False) -> dict:
+    if debug:
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(message)s')
+    else:
+        logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
+
     m_string = decimal_to_fixed_point(m, length, 0)
     r_string = decimal_to_fixed_point(r, length, 0)
 
-    ilen = length + length + 1 #The common length of internal variables
-    a = m_string + '0' * (length + 1) #A: place M in leftmost position. Fill the left bits with 0.
-    s = two_comp(m_string) + '0' * (length + 1) #S: place negative M in leftmost position.
-    p = '0' * (length + 1) + r_string + '0' #P: place R by rightmost 0.
+    ilen = length + length + 1 # The common length of internal variables
+    a = m_string + '0' * (length + 1) # A: place M in leftmost position. Fill the left bits with 0.
+    s = two_comp(m_string) + '0' * (length + 1) # S: place negative M in leftmost position.
+    p = '0' * (length + 1) + r_string + '0' # P: place R by rightmost 0.
 
-    print('Internal variables:')
-    print(f'M = {m}')
-    print(f'R = {r}')
-    print(f'A = {a}')
-    print(f'S = {s}')
-    print(f'P = {p}')
+    logging.debug('Internal variables:')
+    logging.debug(f'M = {m}')
+    logging.debug(f'R = {r}')
+    logging.debug(f'A = {a}')
+    logging.debug(f'S = {s}')
+    logging.debug(f'P = {p}')
 
-    for i in range(length):   #Do operation length times
-        print(f'Step {i+1}:')
+    p_init = p
+    ops = []
+    iterations = []
+
+    for i in range(length):
+        logging.debug(f'Step {i+1}:')
 
         op = p[-2:]
-        print(f'\tThe last 2 bits of p are: {"".join(op)}')
+        ops.append(op)
+
+        logging.debug(f'\tThe last 2 bits of p are: {"".join(op)}')
         if op == '10':
-            print('\tP = (P+S) >> 1')
+            logging.debug('\tP = (P+S) >> 1')
             p = bit_add(p, s, len(p))
         elif op == '01':
-            print('\tP = (P+A) >> 1')
+            logging.debug('\tP = (P+A) >> 1')
             p = bit_add(p, a, len(p))
         elif op == '00':
-            print('\tP = P >> 1')
+            logging.debug('\tP = P >> 1')
         elif op == '11':
-            print('\tP = P >> 1')
+            logging.debug('\tP = P >> 1')
+
+        iterations.append(p)
 
         p = bit_shift(p, 1)
-        print(f'\tP = {p}\n')
+        logging.debug(f'\tP = {p}\n')
 
     p = p[:-1]
-    print(f'The answer is: {p}, {fixed_point_to_decimal(p[-4:], length, 0)}')
-    print(f'Verification: {m} * {r} = {m * r}')
-    print(f'Verification: {m_string} * {r_string} = {p[-4:]}')
+    iterations.append(p)
+
+    logging.debug(f'The answer is: {p}, {fixed_point_to_decimal(p[-4:], length, 0)}')
+    logging.debug(f'Verification: {m} * {r} = {m * r}')
+    logging.debug(f'Verification: {m_string} * {r_string} = {p[-4:]}')
+
+    return {
+        'm' : m,
+        'r' : r,
+        'm_string' : m_string,
+        'r_string' : r_string,
+        'two_comp_m' : two_comp(m_string),
+        'a' : a,
+        's' : s,
+        'p_init' : p_init,
+        'ops' : ops,
+        'iterations' : iterations,
+    }
+
+def main():
+    booth_algo(3, 2, 4, debug=True)
 
 if __name__ == '__main__':
-    booth_algo(3, 2, 4)
+    main()
