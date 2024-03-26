ace136f83c1ff29b880a8878d22d67294f6f585f
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-26 11:36:21 -0400

    version change

--- fixed-library/fixed-point-models/setup.py
+++ fixed-library/fixed-point-models/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 requirements = ['numpy']
 
 setup(name='fixed_point_models',
-    version='0.1.1',
+    version='0.1.2',
     description='Modeling fixed point arithmetic',
     packages=['fixed_point_models'],
     install_requires=requirements
