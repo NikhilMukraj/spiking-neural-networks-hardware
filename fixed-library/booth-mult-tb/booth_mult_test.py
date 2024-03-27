a36b2b644f04fb4fcb2df2268b89edb338d67a04
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-27 18:07:22 -0400

    start of booth tb

--- fixed-library/booth-mult-tb/booth_mult_test.py
+++ fixed-library/booth-mult-tb/booth_mult_test.py
@@ -1,2 +1,27 @@
 # check if all internal variables are correct
 # test reseting with rst and then doing next multiplication
+import cocotb
+from cocotb.triggers import Timer
+from cocotb.binary import BinaryValue
+from fixed_point_models import fixed_point_to_decimal, decimal_to_fixed_point
+from fixed_point_models import booth_algo, check_with_tolerance
+import numpy as np
+
+
+@cocotb.test()
+async def mult_test(dut):
+    int_bits = 16
+    frac_bits = 16
+    lower_bound = -128
+    upper_bound = 128
+
+    for i in range(100):
+        a = np.random.uniform(lower_bound, upper_bound)
+        b = np.random.uniform(lower_bound, upper_bound)
+
+        booth_verification = booth_algo(a, b, length=int_bits + frac_bits)
+
+        # dut.a.value
+        # dut.s.value
+        # dut.p.value
+        # dut.two_comp_m.value
