5361f4dab9648c3272fa342e8fd402b8acc41ac6
Nikhiljag <nikhilmukraj@gmail.com>
2024-03-28 08:15:50 -0400

    clock

--- fixed-library/booth-mult-tb/booth_mult_test.py
+++ fixed-library/booth-mult-tb/booth_mult_test.py
@@ -8,12 +8,21 @@ from fixed_point_models import booth_algo, check_with_tolerance
 import numpy as np
 
 
+async def generate_clock(dut, timesteps):
+    for cycle in range(timesteps):
+        dut.clk.value = 0
+        await Timer(4, units='ns')
+        dut.clk.value = 1
+        await Timer(4, units='ns')
+
 @cocotb.test()
 async def mult_test(dut):
     int_bits = 16
     frac_bits = 16
     lower_bound = -128
     upper_bound = 128
+
+    dut.rst.value = 1
 
     for i in range(100):
         a = np.random.uniform(lower_bound, upper_bound)
