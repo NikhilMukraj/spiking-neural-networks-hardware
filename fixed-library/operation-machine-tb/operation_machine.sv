c34f45b28e75f7becce413c29fed472101c7050a
Nikhiljag <nikhilmukraj@gmail.com>
2024-04-11 15:03:01 -0400

    operation machine

---
+++ fixed-library/operation-machine-tb/operation_machine.sv
@@ -0,0 +1,5 @@
+// determine max number of numbers to store
+// determine which operations to use (can be hardcoded and generated with eq hls)
+// route correct numbers into operation
+// store new numbers, potentially replacing old ones
+// determine next operation
