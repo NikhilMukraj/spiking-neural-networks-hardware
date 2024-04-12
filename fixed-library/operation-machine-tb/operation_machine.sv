abe98f354b411c4ae8c60b10fc2c7f40549055f3
Nikhiljag <nikhilmukraj@gmail.com>
2024-04-12 16:13:33 -0400

    start of operation machine

--- fixed-library/operation-machine-tb/operation_machine.sv
+++ fixed-library/operation-machine-tb/operation_machine.sv
@@ -3,3 +3,24 @@
 // route correct numbers into operation
 // store new numbers, potentially replacing old ones
 // determine next operation
+
+module operation_machine #(
+    parameter N = 32,
+    parameter Q = 16,
+    parameter stack = 5
+)(
+    input clk,
+    input [1:0] operand,
+    input [$clog2(stack)+1:0] index1,
+    input [$clog2(stack)+1:0] index2,
+    input [N-1:0] value,
+    output reg done,
+    output [N-1:0] output
+);
+    // all clocked operations
+    // input constants
+    // choose operand and which operators
+        // if absolute or negator function, choose 0 for other index
+    // add output to stack
+    // output when done
+endmodule
