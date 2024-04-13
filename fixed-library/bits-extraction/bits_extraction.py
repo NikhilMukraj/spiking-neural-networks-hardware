50be391643ac1f23e4b7dfec0402be7497a69fb9
Nikhiljag <nikhilmukraj@gmail.com>
2024-04-13 14:51:33 -0400

    notes

---
+++ fixed-library/bits-extraction/bits_extraction.py
@@ -0,0 +1,5 @@
+# ignore sign for now
+# for booth multplier, find (in code simulation) where desired bit string is
+# find pattern among the indices
+# for mult module, find (in hardware simulation) where desired bit string is
+# find pattern among the indices
