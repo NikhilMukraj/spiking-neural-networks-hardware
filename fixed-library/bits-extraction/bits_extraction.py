c53cb9cc73c787f9cf1de432ad96f81e5d395422
Nikhiljag <nikhilmukraj@gmail.com>
2024-04-14 12:52:15 -0400

    mult find substring

--- fixed-library/bits-extraction/bits_extraction.py
+++ fixed-library/bits-extraction/bits_extraction.py
@@ -3,3 +3,12 @@
 # find pattern among the indices
 # for mult module, find (in hardware simulation) where desired bit string is
 # find pattern among the indices
+
+def find_substring(a, b, out_string, int_bits, frac_bits):
+    a = fixed_point_to_decimal(decimal_to_fixed_point(a, int_bits, frac_bits))
+    b = fixed_point_to_decimal(decimal_to_fixed_point(b, int_bits, frac_bits))
+
+    c = a * b
+    c_string = decimal_to_fixed_point(c_string, int_bits, frac_bits)
+
+    return out_string.find(c_string[1:]) # return match
