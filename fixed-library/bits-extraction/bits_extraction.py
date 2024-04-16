d8f56a71587cdc6fda87e2fe36aee3e72689b6b2
Nikhiljag <nikhilmukraj@gmail.com>
2024-04-16 13:48:54 -0400

    newline

--- fixed-library/bits-extraction/bits_extraction.py
+++ fixed-library/bits-extraction/bits_extraction.py
@@ -9,6 +9,7 @@ def find_substring(a, b, out_string, int_bits, frac_bits):
     b = fixed_point_to_decimal(decimal_to_fixed_point(b, int_bits, frac_bits))
 
     c = a * b
-    c_string = decimal_to_fixed_point(c_string, int_bits, frac_bits)
+    c_string = decimal_to_fixed_point(c, int_bits, frac_bits)
 
     return out_string.find(c_string[1:]) # return match
+    
