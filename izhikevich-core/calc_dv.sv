0e545ab7892aa880ac32e9d5b65dc3be2e5c143b
Nikhiljag <nikhilmukraj@gmail.com>
2024-04-20 10:16:04 -0400

    bits changes

--- izhikevich-core/calc_dv.sv
+++ izhikevich-core/calc_dv.sv
@@ -10,13 +10,13 @@ module calc_dv #(
 );
 	wire [N-1:0] term1, term2, term3, term4, term5, term6, term7, term8;
 
-	mult multiplier1 ( v, v, term1 ); // v*v
-	mult multiplier2 ( 32'b00000000000000000000101000111101, term1, term2 ); // 0.04*(v*v)
-	mult multiplier3 ( 32'b00000000000001010000000000000000, v, term3 ); // 5*v
-	add adder1 ( term2, term3, term4 ); // (0.04*(v*v))+(5*v)
-	negator negator4 ( w, term5 ); // -1*w
-	add adder2 ( term4, 32'b00000000100011000000000000000000, term6 ); // ((0.04*(v*v))+(5*v))+140
-	add adder3 ( term5, i, term7 ); // (-1*w)+i
-	add adder4 ( term6, term7, term8 ); // (((0.04*(v*v))+(5*v))+140)+((-1*w)+i)
-	mult multiplier4 ( term8, step, out ); // ((((0.04*(v*v))+(5*v))+140)+((-1*w)+i))*step
+	mult #(.N(N), .Q(Q)) multiplier1 ( v, v, term1 ); // v*v
+	mult #(.N(N), .Q(Q)) multiplier2 ( 32'b00000000000000000000101000111101, term1, term2 ); // 0.04*(v*v)
+	mult #(.N(N), .Q(Q)) multiplier3 ( 32'b00000000000001010000000000000000, v, term3 ); // 5*v
+	add #(.N(N), .Q(Q)) adder1 ( term2, term3, term4 ); // (0.04*(v*v))+(5*v)
+	negator #(.N(N), .Q(Q)) negator4 ( w, term5 ); // -1*w
+	add #(.N(N), .Q(Q)) adder2 ( term4, 32'b00000000100011000000000000000000, term6 ); // ((0.04*(v*v))+(5*v))+140
+	add #(.N(N), .Q(Q)) adder3 ( term5, i, term7 ); // (-1*w)+i
+	add #(.N(N), .Q(Q)) adder4 ( term6, term7, term8 ); // (((0.04*(v*v))+(5*v))+140)+((-1*w)+i)
+	mult #(.N(N), .Q(Q)) multiplier4 ( term8, step, out ); // ((((0.04*(v*v))+(5*v))+140)+((-1*w)+i))*step
 endmodule
--- izhikevich-core/calc_dw.sv
+++ izhikevich-core/calc_dw.sv
@@ -11,9 +11,9 @@ module calc_dw #(
 );
 	wire [N-1:0] term1, term2, term3, term4;
 
-	mult multiplier1 ( b, v, term1 ); // b*v
-	negator negator2 ( w, term2 ); // -1*w
-	add adder1 ( term1, term2, term3 ); // (b*v)+(-1*w)
-	mult multiplier2 ( a, term3, term4 ); // a*((b*v)+(-1*w))
-	mult multiplier3 ( term4, step, out ); // (a*((b*v)+(-1*w)))*step
+	mult #(.N(N), .Q(Q)) multiplier1 ( b, v, term1 ); // b*v
+	negator #(.N(N), .Q(Q)) negator2 ( w, term2 ); // -1*w
+	add #(.N(N), .Q(Q)) adder1 ( term1, term2, term3 ); // (b*v)+(-1*w)
+	mult #(.N(N), .Q(Q)) multiplier2 ( a, term3, term4 ); // a*((b*v)+(-1*w))
+	mult #(.N(N), .Q(Q)) multiplier3 ( term4, step, out ); // (a*((b*v)+(-1*w)))*step
 endmodule
