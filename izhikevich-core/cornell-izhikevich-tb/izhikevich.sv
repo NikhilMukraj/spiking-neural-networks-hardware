7e68aa87d389387bcede96cf3301dd29f00d9c06
Nikhiljag <nikhilmukraj@gmail.com>
2024-05-21 08:55:34 -0400

    c14

--- izhikevich-core/cornell-izhikevich-tb/izhikevich.sv
+++ izhikevich-core/cornell-izhikevich-tb/izhikevich.sv
@@ -30,6 +30,7 @@ module izhikevich_core #(
     input signed [N-1:0] v_init, // 18'sh3_4CCD ; // -0.7
     input signed [N-1:0] u_init, // 18'sh3_CCCD ; // -0.2
     input signed [N-1:0] v_th, // 18'sh0_4CCC ; // 0.30
+	input signed [N-1:0] c14, // 18'sh1_6666; // 1.4
     input signed [N-1:0] i,
     input signed [N-1:0] a,
     input signed [N-1:0] b,
@@ -39,12 +40,12 @@ module izhikevich_core #(
     output reg signed [N-1:0] u,
     output reg is_spiking,
 );
-	reg signed [17:0] v1,u1;
-	wire signed	[17:0] u1reset, v1new, u1new, du1;
-	wire signed	[17:0] v1xv1, v1xb;
-	wire signed	[17:0] p, c14;
+	reg signed [N-1:0] v1,u1;
+	wire signed	[N-1:0] u1reset, v1new, u1new, du1;
+	wire signed	[N-1:0] v1xv1, v1xb;
+	wire signed	[N-1:0] p, c14;
 	
-	assign c14 = 18'sh1_6666; // 1.4
+	// assign c14 = 18'sh1_6666; // 1.4
 	
 	assign voltage = v1;  // copy state var to output
     assign u = u1;
