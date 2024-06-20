7ecdff01d0a360f9809a5411fee71405886272e2
Nikhiljag <nikhilmukraj@gmail.com>
2024-06-20 13:17:13 -0400

    args changes

--- spike-trains/poisson-neuron/poisson-tb/poisson.sv
+++ spike-trains/poisson-neuron/poisson-tb/poisson.sv
@@ -1,6 +1,9 @@
 module poisson #(
     parameter N=18,
 )(
+    input clk,
+    input apply,
+    input [N-1:0] seed,
     input [N-1:0] chance_of_spiking,
     input [N-1:0] height,
     output reg [N-1:0] voltage,
